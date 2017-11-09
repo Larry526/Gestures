04a72e676ecb8283b056bda501912e7d04a187c3
Larry Luk <larryluk526@gmail.com>
2017-11-09 13:35:27 -0500

    Initial Commit

---
+++ Gestures/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Gestures
+//
+//  Created by Larry Luk on 2017-11-09.
+//  Copyright © 2017 App App Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
