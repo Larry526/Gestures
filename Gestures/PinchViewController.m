e33f06335dfaf19768b7ad3ab8bc179b67965a35
Larry Luk <larryluk526@gmail.com>
2017-11-09 18:00:23 -0500

    tap done

--- Gestures/PinchViewController.m
+++ Gestures/PinchViewController.m
@@ -14,24 +14,25 @@
 
 @implementation PinchViewController
 
-- (void)viewDidLoad {
+-(void)viewDidLoad {
+    
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    CGFloat width = 100;
+    CGFloat height = 100;
+    CGRect frame = CGRectMake(CGRectGetMidX(self.view.bounds) - width/2, CGRectGetMidY(self.view.bounds) - height/2, width, height);
+    
+    UIView *boxView = [[UIView alloc]initWithFrame:frame];
+    boxView.backgroundColor = [UIColor purpleColor];
+    [self.view addSubview:boxView];
+    
+    UIPinchGestureRecognizer *pinchGesture = [[UIPinchGestureRecognizer alloc]initWithTarget:self action:@selector(viewPinched:)];
+    [boxView addGestureRecognizer:pinchGesture];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)viewPinched:(UIPinchGestureRecognizer*)sender{
+    CGFloat scale = sender.scale;
+    sender.view.transform = CGAffineTransformMakeScale(scale, scale);
+    scale = 1.0;
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
--- Gestures/TapViewController.m
+++ Gestures/TapViewController.m
@@ -14,7 +14,29 @@
 
 @implementation TapViewController
 
+-(void)viewDidLoad {
+    [super viewDidLoad];
+    
+    CGFloat width = 100;
+    CGFloat height = 100;
+    CGRect frame = CGRectMake(CGRectGetMidX(self.view.bounds) - width/2, CGRectGetMidY(self.view.bounds) - height/2, width, height);
+    UIView *boxView = [[UIView alloc]initWithFrame:frame];
+    boxView.backgroundColor = [UIColor brownColor];
+    [self.view addSubview:boxView];
+    
+    
+    UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(viewTapped:)];
+    [boxView addGestureRecognizer:tapGesture];
+}
 
 
+-(void)viewTapped:(UITapGestureRecognizer*)sender {
+    
+    if (sender.state == UIGestureRecognizerStateEnded) {
+        UIColor *color = [sender.view.backgroundColor isEqual: [UIColor brownColor]] ? [UIColor blackColor] : [UIColor brownColor];
+        sender.view.backgroundColor = color;
+    }
+    
+}
 
 @end
